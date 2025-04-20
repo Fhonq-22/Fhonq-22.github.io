fd2fa1f3dc70daa94356b805f1dd7b7682e72bdd
Fhonq-22 <hgfog8310a1@gmail.com>
2025-04-20 07:21:21 +0700

    cập nhật

--- js/script.js
+++ js/script.js
@@ -88,7 +88,13 @@ setTimeout(() => {
 
 window.addEventListener('resize', updateMenuHeights);
 
+
+
 const swatches = document.querySelectorAll('.theme-swatch');
+const toggleBtn = document.querySelector('.theme-toggle');
+let hidden = false;
+
+// Bấm vào swatch nào thì đổi theme
 swatches.forEach(swatch => {
   swatch.addEventListener('click', () => {
     const theme = swatch.dataset.theme;
@@ -96,3 +102,17 @@ swatches.forEach(swatch => {
     document.documentElement.classList.add(theme);
   });
 });
+
+// Ẩn/hiện các swatch có hiệu ứng
+toggleBtn.addEventListener('click', () => {
+  hidden = !hidden;
+  swatches.forEach(swatch => {
+    swatch.classList.toggle('hide', hidden);
+  });
+  
+  // Thay đổi icon khi nhấn vào toggle
+  const icon = toggleBtn.querySelector('i');
+  icon.classList.toggle('bx-chevron-left', hidden);
+  icon.classList.toggle('bx-chevron-right', !hidden);
+});
+
